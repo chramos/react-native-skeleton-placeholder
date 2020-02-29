f7d362c405f950abb7222adda0969ff6739f5255
Henrique Ramos <chenrique.ramos12@gmail.com>
2020-02-29 11:44:37 -0300

    Update readme; Added example;

--- example/App.js
+++ example/App.js
@@ -2,37 +2,16 @@ import React from 'react';
 import {SafeAreaView, Text, StyleSheet, View} from 'react-native';
 import FirstExample from './src/FirstExample';
 import SecondExample from './src/SecondExample';
-import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
 
 const App = ({}) => (
   <SafeAreaView style={style.container}>
     <Text style={style.heading}>First example</Text>
-    <Test />
+    <FirstExample />
     <View style={style.separator} />
-
     <Text style={style.heading}>Second example</Text>
     <SecondExample />
   </SafeAreaView>
 );
-
-const Test = () => {
-  return (
-    <SkeletonPlaceholder>
-      <SkeletonPlaceholder.Item flexDirection="row" alignItems="center">
-        <SkeletonPlaceholder.Item width={60} height={60} borderRadius={50} />
-        <SkeletonPlaceholder.Item marginLeft={20}>
-          <SkeletonPlaceholder.Item width={120} height={20} borderRadius={4} />
-          <SkeletonPlaceholder.Item
-            marginTop={6}
-            width={80}
-            height={20}
-            borderRadius={4}
-          />
-        </SkeletonPlaceholder.Item>
-      </SkeletonPlaceholder.Item>
-    </SkeletonPlaceholder>
-  );
-};
 
 const style = StyleSheet.create({
   container: {
